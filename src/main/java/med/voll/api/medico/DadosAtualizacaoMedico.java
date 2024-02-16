945fd2b699344d51d68f770a9894187735e1b34f
Tiago Bastos <tbbgoncalves@outlook.com>
2024-02-16 11:47:10 -0300

    validando os campos na atualização de médico e paciente

--- src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -1,10 +1,16 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosAtualizacaoMedico(@NotNull Long id,
+                                     @NotBlank
                                      String nome,
+                                     @NotBlank
+                                     @Pattern(regexp = "\\d{10,11}")
                                      String telefone,
-                                     DadosEndereco endereco) {
+                                     @Valid DadosEndereco endereco) {
 }
